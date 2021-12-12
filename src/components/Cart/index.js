1e482130807f0f3c41dd7703440724db2205b4fd
Monil Diwan <monildiwan@Monils-MacBook-Air.local>
2021-12-12 00:22:18 -0500

    Jose

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,10 +2,28 @@ import React from 'react';
 import NavigationTop from "../NavigationTop/index";
 import Footer from "../Footer/Footer";
 import CartComponent from "./CartComponent";
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
 const Cart = () => {
+    const history = useHistory();
+    const authListener= () => { 
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if(user) {
+                
+            } else {
+                history.push("/");
+            }
+        });
+    }
+    useEffect(() => {
+        authListener();
+    }, []);
     return (
         <>
-            <NavigationTop/>
+            <NavigationTop isLoggedIn={true}/>
             <CartComponent/>
             <Footer/>
         </>
